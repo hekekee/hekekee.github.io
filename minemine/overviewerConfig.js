6b6be39671da326e595bd2063b7a65737191260d
Heikki Stark <heikkistark@gmail.com>
2020-01-14 19:02:37 +0200

    minemine

---
+++ minemine/overviewerConfig.js
@@ -0,0 +1,67 @@
+var overviewerConfig = {
+    "CONST": {
+        "tileSize": 384,
+        "image": {
+            "defaultMarker": "signpost.png",
+            "signMarker": "signpost_icon.png",
+            "bedMarker": "bed.png",
+            "spawnMarker": "icons/marker_home.png",
+            "spawnMarker2x": "icons/marker_home_2x.png",
+            "queryMarker": "icons/marker_location.png",
+            "queryMarker2x": "icons/marker_location_2x.png",
+            "compass": {
+                "0": "compass_upper-left.png",
+                "1": "compass_upper-right.png",
+                "3": "compass_lower-left.png",
+                "2": "compass_lower-right.png"
+            }
+        },
+        "mapDivId": "mcmap",
+        "UPPERLEFT": 0,
+        "UPPERRIGHT": 1,
+        "LOWERLEFT": 3,
+        "LOWERRIGHT": 2
+    },
+    "worlds": [
+        "2020 Minehaaste"
+    ],
+    "map": {
+        "debug": true,
+        "cacheTag": "1579021180",
+        "north_direction": "lower-left",
+        "controls": {
+            "pan": true,
+            "zoom": true,
+            "spawn": true,
+            "compass": true,
+            "mapType": true,
+            "overlays": true,
+            "coordsBox": true
+        }
+    },
+    "tilesets": [
+        {
+            "name": "Best minehaaste ever",
+            "zoomLevels": 7,
+            "defaultZoom": 3,
+            "maxZoom": 7,
+            "path": "normalrender",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "2020 Minehaaste",
+            "last_rendertime": 1579013128,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -1123,
+                64,
+                -524
+            ],
+            "minZoom": 2,
+            "spawn": false,
+            "north_direction": 0
+        }
+    ]
+};
