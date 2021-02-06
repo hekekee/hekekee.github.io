ad05ec901b1929f1b6ab60289c3ecf69e5adc7f7
hekekee <heikkistark@gmail.com>
2021-02-06 02:08:22 +0200

    Mineminemine

--- minemine/overviewerConfig.js
+++ minemine/overviewerConfig.js
@@ -27,7 +27,7 @@ var overviewerConfig = {
     ],
     "map": {
         "debug": false,
-        "cacheTag": "1612473174",
+        "cacheTag": "1612569604",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
@@ -49,7 +49,7 @@ var overviewerConfig = {
             "base": "",
             "bgcolor": "#1a1a1a",
             "world": "Mine-tuottis2021",
-            "last_rendertime": 1612471434,
+            "last_rendertime": 1612569143,
             "imgextension": "png",
             "isOverlay": false,
             "poititle": "Markers",
